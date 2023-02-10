41345916a6f517ad0c8df17b95178058c6604404
Ktuna95 <hakunamatata95@naver.com>
2023-02-10 17:35:20 +0900

    Updates

---
+++ precache-manifest.dde999dccaab1ca1a8135ccdc3e4bc0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb939f68fad5382d0106f602351f524",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "36644f0d71de0edfae67",
+    "url": "/Todolist/static/js/2.7c7fb809.chunk.js"
+  },
+  {
+    "revision": "6960e058a1ab0fad50d5",
+    "url": "/Todolist/static/js/main.8715601d.chunk.js"
+  },
+  {
+    "revision": "f96ff8426624f7c2968c",
+    "url": "/Todolist/static/js/runtime~main.532f3e5b.js"
+  }
+]);
